67eb8158bf482dec8f43d6465d4472a33001b231
Jenniffer <jen.cubillos@gmail.com>
2016-12-18 17:36:36 +0100

    Initial commit

---
+++ js/game.js
@@ -0,0 +1,7 @@
+
+// Only executed our code once the DOM is ready.
+window.onload = function() {
+  // Get a reference to the canvas object
+  var canvas = document.getElementById('gameCanvas');
+  console.log('test');
+}
